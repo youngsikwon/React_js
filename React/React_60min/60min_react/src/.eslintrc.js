6899863a1daf3672bc5849f52735690a72e0de31
youngsikwon <youngsik3200@gmail.com>
2021-02-08 21:59:46 +0900

    React 개발환경

---
+++ React/React_60min/60min_react/src/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports= {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+    rules: {
+        'react/jsx-filename-extension': ['error',
+    {
+        'extensions': [".js", ".jsx"]
+    }
+    ]
+    }
+};
